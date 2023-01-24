f18592a8fcc0f90bb73419681ebc2210dbe36e0e
Rene Krieg <rene_k@me.com>
2023-01-24 16:06:21 +0100

    Change default zsh to brew version

---
+++ setup_zsh.zsh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting ZSH Setup >>>\n"
+
+# Installation unnecessary; it's in the Brewfile.
+
+# https://stackoverflow.com/a/4749368/1341838
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+  echo '/opt/homebrew/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
+
+
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo '$SHELL is already /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
+# Change default script shell to zsh
+
+# if sh --version | grep -q zsh; then
+#   echo '/private/var/select/sh already linked to /bin/zsh'
+# else
+#   echo "Enter superuser (sudo) password to symlink sh to zsh"
+#   # Looked cute, might delete later, idk
+#   sudo ln -sfv /bin/zsh /private/var/select/sh
+
+#   # I'd like for this to work instead.
+#   # sudo ln -sfv /opt/homebrew/bin/zsh /private/var/select/sh
+# fi
