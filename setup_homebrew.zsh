006f4baea22399382bee68966c2f62dc93017a27
Rene <94539924+renekrieg@users.noreply.github.com>
2024-05-29 13:47:55 +0200

    Update setup_homebrew.zsh

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -9,6 +9,7 @@ else
  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
  echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/$(whoami)/.zprofile
  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
+ eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Since the new direction of homebrew is not part of the default `PATH` we need to add it. The easiest way to do that is to follow the instructions Homebrew spits out after installation.
